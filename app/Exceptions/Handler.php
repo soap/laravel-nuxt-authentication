c363b06cbb72f6ee70c9e76f63357a72ca8315f1
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2021-09-15 01:04:34 +0700

    Update laravel framework from 5.8 to 7.0

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,11 +7,12 @@ use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
-   // use Throwable - you should NOT import Throwable class as a trait here. You need to just import it above the class
+    // use Throwable - you should NOT import Throwable class as a trait here. You need to just import it above the class
     public function report(Throwable $exception)
     {
         parent::report($exception);
     }
+
     public function render($request, Throwable $exception)
     {
         return parent::render($request, $exception);
